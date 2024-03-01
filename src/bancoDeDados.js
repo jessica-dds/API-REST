2d05f6ac657b4fd781f10cd3dbda9e1bff95ae9f
jessica-dds <jessica.gleizer@hotmail.com>
2024-02-29 21:47:17 -0300

    API REST

---
+++ src/bancoDeDados.js
@@ -0,0 +1,20 @@
+const bancoDeDados = {
+    identificadorInstrutor: 3,
+    instrutores: [
+        {
+            id: 1,
+            nome: 'Jéssica',
+            email: 'jessica.gleizer@hotmail.com',
+            status: true
+        },
+
+        {
+            id: 2,
+            nome: 'Dani',
+            email: 'dani@email.com',
+            status: true
+        }
+    ]
+}
+
+module.exports = bancoDeDados
